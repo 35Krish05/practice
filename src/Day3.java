07e12fa60dd067cc69bc95015cc390f96764426b
Krish Patel <krish@gmail.com>
2025-12-24 16:48:05 +0530

    Initial commit

---
+++ src/Day3.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Day3 {
+    public static void main(String[] args) {
+//control flow - conditional statement
+         //if if-else if-else-if lader
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your marks : ");
+        int marks = sc.nextInt();
+      if(marks >= 35 ) {
+          System.out.println("Pass and ");
+          if (marks >= 80) {
+              System.out.println("GRADE = A");
+          } else if (marks >= 60 && marks < 80) {
+              System.out.println("GRADE = B");
+          } else {
+              System.out.println("GRADE = C");
+          }
+      }
+        else {
+              System.out.println("Fail ");
+          }
+//Nested if statements
+       if (marks >= 35) {
+           if (marks >= 90 ){
+               System.out.println("You got prize iphone");
+           }else {
+               System.out.println("you got Nothing");
+           }
+       }
+
+//Ternary operator
+         int a=10,b=20;
+         int max = (a<b) ? b : a ;
+        System.out.println("Maximum number is : " + max);
+
+//switch case statements (including new enhanced switch from java 14+)
+        int day = 3;
+
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            default -> System.out.println("Invalid day");
+        }
+
+    }
+}
