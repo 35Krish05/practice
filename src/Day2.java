c66465a81c6f1a44ad2f0449eb45af9256f734f8
Krish Patel <krish@gmail.com>
2025-12-24 15:21:09 +0530

    Initial commit

---
+++ src/Day2.java
@@ -0,0 +1,13 @@
+public class Day2 {
+    public static void main(String[] args) {
+     int a = 10;
+     float f = 1.2f;
+     double d = 1.2323;
+     char c = 'k';
+     boolean b = true;
+     //type casting
+        //implicit
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("hey!\nMy name is krish");
+        System.out.println("hello\twhat are you doing ");
+        int age=21;
+        String name = "Krish";
+        String hobby = "playing physical games";
+        System.out.println("My age is : " + age);
+        System.out.println("My name is : " + name);
+        System.out.println("My favourite hobby is : " + hobby);
+    }
+}
