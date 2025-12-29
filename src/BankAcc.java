8550c54f7b6902d13680999879734245c0e6cd78
Krish Patel <krish@gmail.com>
2025-12-29 14:16:14 +0530

    Initial commit

--- src/BankAcc.java
+++ src/BankAcc.java
@@ -1,10 +1,14 @@
 public class BankAcc {
     private double balance = 50000;
     public void withdraw(double amount) throws InsufficientBalanceException{
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than zero");
+        }
         if (amount > balance){
             throw new InsufficientBalanceException("Your balance is Insufficient");
         }
         balance -= amount;
+        System.out.println("Withdrawal successful!");
         System.out.println("Now your balance is : " + balance);
 
     }
--- src/Day11.java
+++ src/Day11.java
@@ -1,19 +1,31 @@
+import java.util.InputMismatchException;
+
 import java.util.Scanner;
 
 public class Day11 {
     public static void main(String[] args) {
      BankAcc b = new BankAcc();
-        System.out.println("Enter amount that you want to withdraw : ");
+
         Scanner sc = new Scanner(System.in);
-        double money = sc.nextDouble();
         try {
+            System.out.println("Enter amount that you want to withdraw : ");
+            double money = sc.nextDouble();
             b.withdraw(money);
+        }catch (InputMismatchException e) {
+            System.out.println("Invalid input! Please enter numbers only.");
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }catch (InsufficientBalanceException e){
             e.printStackTrace();
         }
-
-
-
+        catch (Exception e) {
+            System.out.println("❌ Something went wrong. Please try again.");
+        }
+        finally {
+            sc.close();
+            System.out.println("Program finished safely.");
+        }
     }
 }
 
